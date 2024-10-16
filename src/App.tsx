e192896d1fc9c63fb29193b59b1860d836a5c177
Akshay Krishna <akshay.krishna@qburst.com>
2024-10-16 12:32:05 +0530

    moved json to public

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
 
   useEffect(() => {
     const getData = () => {
-      fetch(`src/layout${num}.json`)
+      fetch(`/layout${num}.json`)
         .then(function (response) {
           return response.json();
         })
